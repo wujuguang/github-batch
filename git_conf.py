9db9e9456448b4dbbada7213460ff5f4d80c41a1
wujuguang <1154545932@qq.com>
2016-03-06 16:56:46 +0800

    代码初始入库.

---
+++ git_conf.py
@@ -0,0 +1,14 @@
+#!usr/bin/env python
+# coding: utf-8
+import os
+
+third_party_dir = "/home/kylin/GitHub/third-party/"
+
+git_fork_dir = [
+    os.path.join(third_party_dir, "Python"),
+    os.path.join(third_party_dir, "Php"),
+    os.path.join(third_party_dir, "System"),
+    os.path.join(third_party_dir, "Online-Docs"),
+]
+
+mux_plug_dir = "/home/kylin/Program/kytmux/tmux/plugins"
---
+++ git_main.py
@@ -0,0 +1,27 @@
+#!usr/bin/env python
+# coding: utf-8
+
+import os
+import os.path
+from git_batch import GitTool
+from git_conf import git_fork_dir, mux_plug_dir
+
+if __name__ == '__main__':
+    """运行实例.
+    """
+
+    logs_dir = os.path.join(os.path.abspath(os.path.dirname(__file__)), "log")
+
+    report_log = os.path.join(logs_dir, "git.log")
+    shells_str = "git pull upstream master:master >> %(log)s && git push origin master:master >> %(log)s"
+    shells_str = ''.join(("cd %s && ", shells_str % {"log": report_log}))
+
+    repository = GitTool(parent_path=git_fork_dir, shells=shells_str, log=report_log)
+    repository()
+
+    report_log = os.path.join(logs_dir, "plugins.log")
+    shells_str = "git pull >> %(log)s"
+    shells_str = ''.join(("cd %s && ", shells_str % {"log": report_log}))
+
+    repository = GitTool(parent_path=mux_plug_dir, shells=shells_str, log=report_log)
+    repository()
